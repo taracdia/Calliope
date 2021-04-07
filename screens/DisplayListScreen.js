7609caac209566634e0d6df8eaed6c45f166d522
Tara Dia <taracdia@gmail.com>
2021-04-07 14:44:38 -0500

    DisplayList early set up

--- screens/DisplayListScreen.js
+++ screens/DisplayListScreen.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+	View,
+	Text,
+	FlatList,
+	Image,
+	TouchableOpacity,
+	StyleSheet,
+} from "react-native";
 import ScreenChanger from "../components/ScreenChanger";
+import songs from "../songs.json";
 
 const DisplayList = () => {
 	return (
 		<View>
-			<Text>List</Text>
+			<FlatList
+				data={songs}
+				keyExtractor={item => item.id.toString()}
+				// ItemSeparatorComponent={() => Separator()}
+				renderItem={({ item }) => (
+					<View style={{ flexDirection: "row" }}>
+						<Text>{item.title}</Text>
+						<Text>{item.artist}</Text>
+						{/* TODO: add other parts */}
+					</View>
+				)}
+			/>
 			<ScreenChanger />
 		</View>
 	);
